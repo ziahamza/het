9785f03679d71ea95b822d4f91ccd8d01f6a692f
hamza zia <ziahamza2007@gmail.com>
2014-09-29 12:28:42 +0800

    added stop words

---
+++ stemmer.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"fmt"
+	"log"
+
+	"github.com/reiver/go-porterstemmer"
+
+	"io/ioutil"
+	"strings"
+)
+
+func main() {
+	stopWords := map[string]bool{}
+	fileData, err := ioutil.ReadFile("stopwords.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	content := string(fileData[:])
+
+	words := strings.Split(content, "\n")
+
+	for _, word := range words {
+		stopWords[word] = true
+	}
+
+	word := ""
+	for true {
+		fmt.Printf("Please enter a single english word: ")
+		fmt.Scanf("%s", &word)
+
+		if len(word) == 0 {
+			break
+		}
+
+		if stopWords[word] {
+			fmt.Printf("It should be stopped\n")
+		} else {
+			stem := porterstemmer.StemString(word)
+
+			fmt.Printf("The stem of %s is %s \n", word, stem)
+		}
+	}
+}
