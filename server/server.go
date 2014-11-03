50386aeb5a6bb17ce1a8e1e5f821b81fd91fa5e6
Wai Man Chan <wmchanad@stu.ust.hk>
2014-11-03 17:45:39 +0800

    Added web server logic

--- server/server.go
+++ server/server.go
@@ -1,3 +1,16 @@
 package main
 
-import "fmt"
+import (
+        "io"
+        "net/http"
+        )
+
+func hello(w http.ResponseWriter, r *http.Request) {
+    
+    io.WriteString(w, "Hello world!")
+}
+
+func main() {
+    http.Handle("/", http.StripPrefix("/", http.FileServer(http.Dir("./static/"))))
+    http.ListenAndServe(":8000", nil)
+}
